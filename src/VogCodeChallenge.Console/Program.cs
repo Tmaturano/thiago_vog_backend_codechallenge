ba558865e7c5c363b0ef390b17d3efb925402ac8
Thiago Maturana <thiagomatu@gmail.com>
2020-11-21 16:16:20 -0300

    [question7] added the console project and created the Question class

---
+++ src/VogCodeChallenge.Console/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace VogCodeChallenge.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            QuestionClass.IterateList();
+        }
+    }
+
+    public static class QuestionClass
+    {
+        static List<string> NamesList = new List<string>()
+        {
+            "Jimmy",
+            "Jeffrey",
+            "John"
+        };
+
+        // Iterate through the NamesList items without using ForEach/For loops.
+        public static void IterateList()
+        {
+            var count = 0;
+
+            do
+            {
+                System.Console.WriteLine(NamesList[count]);
+                count++;
+            } while (count <= NamesList.Count -1);
+        }       
+    }
+}
